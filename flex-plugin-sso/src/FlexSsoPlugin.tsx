c10c8bcb785a66913d18c6e2511c6e33f1c581b6
Bruno Kilian <bkilian@twilio.com>
2022-03-08 23:17:59 +0100

    add: Remove Flex Insights if not Internal Employee

--- flex-plugin-sso/src/FlexSsoPlugin.tsx
+++ flex-plugin-sso/src/FlexSsoPlugin.tsx
@@ -4,7 +4,7 @@ import { FlexPlugin } from '@twilio/flex-plugin';
 import SideBarButton from './components/SideBarButton';
 import Panel from './components/Panel';
 import { View } from '@twilio/flex-ui';
-import { hasManyCompanies } from './helpers/config';
+import { isSupervisor, isWorkerInternal } from './helpers/helpers';
 
 const PLUGIN_NAME = 'FlexSsoPlugin';
 
@@ -13,35 +13,22 @@ export default class FlexSsoPlugin extends FlexPlugin {
     super(PLUGIN_NAME);
   }
 
-  private isSupervisor(manager: Flex.Manager) {
-    // admin role is when you log on Flex from Twilio Console
-    const { attributes } = manager.workerClient;
-    if (attributes.roles.includes('admin')) {
-      return true;
-    }
-
-    // check if the supervisor has "canAddAgents" flag.
-    if (attributes.roles.includes('supervisor') && attributes.canAddAgents) {
-      return true;
-    }
-
-    return false;
-  }
-
-  private applyTeamViewFilters = (flex: typeof Flex, manager: Flex.Manager) => {
-    // if "no bpo concept", if "admin role" or "supervisor role but internal", then no filter is applied
-    const { attributes } = manager.workerClient;
-    const { department_name, roles } = attributes;
-    if (!hasManyCompanies || roles.includes('admin') || department_name === 'internal') {
+  private adjustFlexInsights = (flex: typeof Flex, manager: Flex.Manager) => {
+    if (isWorkerInternal(flex, manager)) {
       return;
     }
 
-    // if department name is null when role = supervisor, something is wrong
-    if (!department_name) {
-      throw new Error('SSO Plugin: Ops, something is wrong. This Worker has no attribute "department_name". How come?!');
-    }
+    // If Supervisor from other companies, remove Flex Insights for now.
+    flex.SideNav.Content.remove('dashboards');
+    flex.SideNav.Content.remove('analyze');
+  };
 
-    // apply the filter
+  private applyTeamViewFilters = (flex: typeof Flex, manager: Flex.Manager) => {
+    const { department_name } = manager.workerClient.attributes;
+
+    if (isWorkerInternal(flex, manager)) {
+      return;
+    }
     flex.TeamsView.defaultProps.hiddenFilter = `data.attributes.department_name == "${department_name}"`;
   };
 
@@ -63,7 +50,7 @@ export default class FlexSsoPlugin extends FlexPlugin {
   }
 
   init(flex: typeof Flex, manager: Flex.Manager) {
-    if (!this.isSupervisor(manager)) {
+    if (!isSupervisor(manager)) {
       return;
     }
 
@@ -78,6 +65,7 @@ export default class FlexSsoPlugin extends FlexPlugin {
     );
 
     this.applyTeamViewFilters(flex, manager);
+    this.adjustFlexInsights(flex, manager);
     this.registerAlerts(flex, manager);
   }
 }
--- flex-plugin-sso/src/helpers/helpers.ts
+++ flex-plugin-sso/src/helpers/helpers.ts
@@ -1,4 +1,5 @@
-import { companies } from './config';
+import * as Flex from '@twilio/flex-ui';
+import { companies, hasManyCompanies } from './config';
 
 export const getCompanyName = (id: string) => {
   if (id === 'internal') {
@@ -10,4 +11,35 @@ export const getCompanyName = (id: string) => {
   }
 
   return '';
+};
+
+export const isWorkerInternal = (flex: typeof Flex, manager: Flex.Manager) => {
+  // if "no bpo concept", if "admin role" or "supervisor role but internal", then no filter is applied
+  const { attributes } = manager.workerClient;
+  const { department_name, roles } = attributes;
+  if (!hasManyCompanies || roles.includes('admin') || department_name === 'internal') {
+    return true;
+  }
+
+  // if department name is null when role = supervisor, something is wrong
+  if (!department_name) {
+    throw new Error('SSO Plugin: Ops, something is wrong. This Worker has no attribute "department_name". How come?!');
+  }
+
+  return false;
+};
+
+export const isSupervisor = (manager: Flex.Manager) => {
+  // admin role is when you log on Flex from Twilio Console
+  const { attributes } = manager.workerClient;
+  if (attributes.roles.includes('admin')) {
+    return true;
+  }
+
+  // check if the supervisor has "canAddAgents" flag.
+  if (attributes.roles.includes('supervisor') && attributes.canAddAgents) {
+    return true;
+  }
+
+  return false;
 };
